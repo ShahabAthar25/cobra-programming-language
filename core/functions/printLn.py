6fd6f02e86f00d02fb28e6425f0b8ef3c55b049f
ShahabAthar25 <shahabathar25@gmail.com>
2022-10-09 14:46:29 +0500

    Starting from scratch

--- core/functions/printLn.py
+++
@@ -1,5 +0,0 @@
-def printLn(current_token):
-        for i in range(len(current_token.get("value"))):
-            print(current_token.get("value")[i].get("value"), end="")
-        
-        print(current_token.get("args").get("endsWith").get("value"), end="")
