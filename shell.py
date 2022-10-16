31cab223a50eb866c1a18466b8c364d4fb844748
ShahabAthar25 <shahabathar25@gmail.com>
2022-10-16 12:55:56 +0500

    Starting from scratch

--- shell.py
+++ shell.py
@@ -1,23 +1,4 @@
-from Core.Interpreter import Interpreter
-from Core.Lexer import Lexer
-from Core.Parser import Parser
-
-
 while True:
-    try:
-        text = input("COBRA >>> ")
+    text = input("COBRA >>> ")
 
-        lexer = Lexer(text)
-        tokens = lexer.generate_tokens()
-
-        parser = Parser(tokens)
-        tree = parser.parse()
-
-        if not tree: continue
-
-        interpreter = Interpreter()
-        value = interpreter.visit(tree)
-
-        print(value)
-    except Exception as error:
-        print(error)
+    print(text)
